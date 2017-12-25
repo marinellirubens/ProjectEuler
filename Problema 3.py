cc5e25eef9492d3bab86f8eadc4804c49c4fceb6
marinellirubens <marinelli.rubens@outlook.com>
2017-12-25 01:44:08 -0200

    corrigido problema 3

--- Problema 3.py
+++ Problema 3.py
@@ -1,38 +1,23 @@
 #-*- encoding: utf-8 -*-
 import math
 import sys
-sys.setrecursionlimit(15000)
+sys.setrecursionlimit(30000)
 PREMISSA='''
 The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ?'''
 
 def main(Number):
-	Square = math.sqrt(Number)
-	MaxNumber = []
-	#print(int(Square))
-	for x in range(3,int(Square),2):
-		#print(x, IsPrime(x,3))
-		if IsPrime(x,3):
-			MaxNumber.append(x)
+	i = 3
+	while i * i < Number:
+		while Number % i == 0:
+			#print(Number)
+			Number = Number / i
+		i = i + 1
+		#print(i)
+	print(Number) 
 
-	result = 0
-	print(result)
-	print(MaxNumber)
-	MaxNumber.reverse()
-	for x in MaxNumber:
-		if Number % x == 0:
-			result = x
-			break
-	print(result)
 
-def IsPrime(NumberPrime, Compare):
-	if NumberPrime <= Compare:
-		return True
-	elif NumberPrime % Compare == 0:
-		return False
-	else:
-		return IsPrime(NumberPrime, Compare + 2)
 
 if __name__ == '__main__':
 	main(600851475143)
